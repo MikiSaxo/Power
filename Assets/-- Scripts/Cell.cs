09e9f72d1a6391d6522a854ecdbfa9788bffa66d
P3D\samuel.charlet <s.ch4rl3t@gmail.com>
2024-01-26 15:50:05 +0100

    Blue Yellow un peu de Red

--- Assets/-- Scripts/Cell.cs
+++ Assets/-- Scripts/Cell.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    public List<string> Neighbor { get; set; } = new List<string>();
+    [field: SerializeField] public Colors Color { get; set; }
+    [field: SerializeField] public bool IsSea { get; set; }
+    [field: SerializeField] public List<Cell> Neighbor { get; set; } = new List<Cell>();
+}
 
+public enum Colors
+{
+    Neutral = -1,
+    Blue = 0,
+    Yellow = 1,
+    Red = 2,
+    Green = 3,
 }
