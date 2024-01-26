7a26af5a4f0cf09b06ea983a415409d106a8792b
P3D\samuel.charlet <s.ch4rl3t@gmail.com>
2024-01-26 16:30:56 +0100

    End Neutral

--- Assets/-- Scripts/Cell.cs
+++ Assets/-- Scripts/Cell.cs
@@ -1,12 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    [field: SerializeField] public Colors Color { get; set; }
+    [field: SerializeField] public Colors CellColor { get; set; }
     [field: SerializeField] public bool IsSea { get; set; }
     [field: SerializeField] public List<Cell> Neighbor { get; set; } = new List<Cell>();
+
+    [Header("---- Prefab")]
+    [SerializeField] private GameObject _img;
+
+
+    private void Start()
+    {
+        if (CellColor == Colors.Yellow)
+        {
+            UpdateViewCell(true);
+            foreach (var cell in Neighbor)
+            {
+                cell.UpdateViewCell(true);
+            }
+        }
+    }
+
+    public void UpdateViewCell(bool state)
+    {
+        _img.SetActive(state);
+    }
 }
 
 public enum Colors
