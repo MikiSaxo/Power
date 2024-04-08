89fcfe1c9de572e77dcbfb2109bce17c3502670d
Samuel <MikiSaxo>
2024-04-08 11:02:53 +0200

    Import Server + update gitignore

---
+++ Server/Serverside Code/Game Code/IFunction.cs
@@ -0,0 +1,15 @@
+﻿using PlayerIO.GameLibrary;
+using SamServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamAssemblyExample
+{
+    internal interface IFunction
+    {
+        void Execute(Player player, Message message, GameCode game);
+    }
+}
---
+++ Server/Serverside Code/Game Code/Test.cs
@@ -0,0 +1,18 @@
+﻿using PlayerIO.GameLibrary;
+using SamServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamAssemblyExample
+{
+    internal class Test : IFunction
+    {
+        public void Execute(Player player, Message message, GameCode game)
+        {
+            Console.WriteLine($"{message.GetInt(0)}  {message.GetString(1)}");
+        }
+    }
+}
